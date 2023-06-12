132ff70c8a714558ed4689045bc136b2961c8ca6
Ruslan <zzzlyka221@gmail.com>
2023-06-12 20:06:58 +0500

    Added game over screen and restart

--- game/game.go
+++ game/game.go
@@ -5,40 +5,47 @@ import (
 	"fmt"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
+	"github.com/hajimehoshi/ebiten/inpututil"
+	"github.com/hajimehoshi/ebiten/text"
+	"golang.org/x/image/font/basicfont"
 	"image/color"
 	_ "image/png"
 	"snake/objects"
 )
 
 type Game struct {
-	food     *objects.Food
-	snake    *objects.Snake
-	running  bool
-	speed    int
-	maxSpeed int
+	food      *objects.Food
+	snake     *objects.Snake
+	isRunning bool
+	isPaused  bool
+	speed     int
+	maxSpeed  int
 }
 
 func NewGame() *Game {
 	return &Game{
-		food:     objects.NewFood(),
-		snake:    objects.NewSnake(),
-		running:  true,
-		speed:    0,
-		maxSpeed: 10,
+		food:      objects.NewFood(),
+		snake:     objects.NewSnake(),
+		isRunning: true,
+		isPaused:  true,
+		speed:     0,
+		maxSpeed:  10,
 	}
 }
 
 func (g *Game) Update(screen *ebiten.Image) error {
+	if g.isRunning == false {
+		if inpututil.IsKeyJustPressed(ebiten.KeyR) {
+			g.Restart()
+		}
+		return nil
+	}
+
 	if g.speed < g.maxSpeed {
 		g.speed += 1
 		return nil
 	}
 	g.speed = 0
-
-	if g.running == false {
-		fmt.Println("Game over!")
-		// todo: stop the game
-	}
 
 	g.snake.Move()
 
@@ -60,7 +67,7 @@ func (g *Game) Update(screen *ebiten.Image) error {
 	}
 
 	if g.CheckGameOver() != nil {
-		g.running = false
+		g.isRunning = false
 	}
 
 	return nil
@@ -68,6 +75,17 @@ func (g *Game) Update(screen *ebiten.Image) error {
 
 func (g *Game) Draw(screen *ebiten.Image) {
 	screen.Fill(color.RGBA{R: 0, G: 0, B: 0, A: 255})
+
+	if g.isRunning == false {
+		face := basicfont.Face7x13
+		restartText := "Game over. Press \"R\" to restart."
+		bounds := text.BoundString(face, restartText)
+		cx := objects.SCREEN_WIDTH / 2
+		cy := objects.SCREEN_HEIGHT / 2
+		x, y := cx-bounds.Min.X-bounds.Dx()/2, cy-bounds.Min.Y-bounds.Dy()/2
+
+		text.Draw(screen, restartText, face, x, y, objects.RED)
+	}
 
 	ebitenutil.DrawRect(screen, float64(g.food.Point.X), float64(g.food.Point.Y), objects.SQUARE_SIZE, objects.SQUARE_SIZE, objects.RED)
 
@@ -83,15 +101,28 @@ func (g *Game) Layout(outsideWidth int, outsideHeight int) (screenWidth int, scr
 func (g *Game) CheckGameOver() error {
 	head := g.snake.Body[0]
 
-	if head.X > objects.SCREEN_WIDTH || head.X < 0 || head.Y > objects.SCREEN_HEIGHT || head.Y < 0 {
+	fmt.Println(head)
+	if head.X > objects.SCREEN_WIDTH-objects.SQUARE_SIZE ||
+		head.X < objects.SQUARE_SIZE ||
+		head.Y > objects.SCREEN_HEIGHT-objects.SQUARE_SIZE ||
+		head.Y < objects.SQUARE_SIZE {
 		return errors.New("snake hit border")
 	}
 
 	for _, bodyPoint := range g.snake.Body[1:] {
 		if head == bodyPoint {
-			return errors.New("snake itself")
+			return errors.New("snake hit itself")
 		}
 	}
 
 	return nil
+}
+
+func (g *Game) Restart() {
+	g.food = objects.NewFood()
+	g.snake = objects.NewSnake()
+	g.isRunning = true
+	g.isPaused = true
+	g.speed = 0
+	g.maxSpeed = 10
 }
